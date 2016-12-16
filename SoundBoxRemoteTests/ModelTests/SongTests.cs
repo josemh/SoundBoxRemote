329886deb0d7f44c772fd2720d6a04cf24fc392d
AntonyCorbett <antony@corbetts.org.uk>
2016-12-16 11:43:19 +0000

    Increased sleep time in tests to accommodate fading images (inc background image) and video songs

--- SoundBoxRemoteTests/ModelTests/SongTests.cs
+++ SoundBoxRemoteTests/ModelTests/SongTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SoundBoxRemoteLib.Models;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace SoundBoxRemoteTests.ModelTests
     [TestClass]
     public class SongTests : BaseModelTest
     {
+        const int SLEEP_MS = 6000;
 
         [TestMethod]
         public void TestLoadSongs()
@@ -18,6 +20,8 @@ namespace SoundBoxRemoteTests.ModelTests
         public void TestSetNewSong()
         {
             Assert.IsTrue(SoundBoxServer.ActiveServer.Songs[0].SetNewSong(100));
+            Assert.IsTrue(SoundBoxServer.ActiveServer.Songs[1].SetNewSong(101));
+            Assert.IsTrue(SoundBoxServer.ActiveServer.Songs[2].SetNewSong(102));
         }
 
         [TestMethod]
@@ -28,7 +32,8 @@ namespace SoundBoxRemoteTests.ModelTests
             Assert.IsFalse(server.Songs[0].PlaySong(), "Same song already playing, should not have played again");
             Assert.IsFalse(server.Songs[1].PlaySong(), "Another song is playing, should not have been allowed");
 
-            Task.Delay(500);
+            Thread.Sleep(SLEEP_MS);
+            
             Assert.IsTrue(server.Songs[0].StopSong(), "Could not stop song");
         }
 
@@ -39,7 +44,7 @@ namespace SoundBoxRemoteTests.ModelTests
             Assert.IsTrue(server.PlayBackgroundMusic());
             Assert.IsFalse(server.PlayBackgroundMusic(), "Music is already playing. This should have failed");
 
-            Task.Delay(500);
+            Thread.Sleep(SLEEP_MS);
 
             Assert.IsTrue(server.StopBackgroundMusic());
             Assert.IsFalse(server.StopBackgroundMusic(), "Music is already stopped. This should have failed");
